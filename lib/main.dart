47740df04bc9697ab0ab34acd9f856fb7be33d54
Avinash174 <avinamshmagar15@gmail.com>
2023-07-11 16:25:16 +0530

    firebase integration done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:assignment_one/screens/sign_screen/sign.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -12,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
